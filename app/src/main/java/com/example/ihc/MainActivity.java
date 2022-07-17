c1f4b47af3c24ab9e63da65d26c3191a52796bdf
acturatti <arturturatti1@gmail.com>
2022-07-17 20:51:46 -0300

    Fix: Atividade 1.3

--- app/src/main/java/com/example/ihc/MainActivity.java
+++ app/src/main/java/com/example/ihc/MainActivity.java
@@ -1,30 +1,70 @@
 package com.example.ihc;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
-    private EditText first_number;
-    private EditText second_number;
+public class MainActivity extends Activity implements SensorEventListener {
+    private TextView view_x, view_y, view_z;
+    private float x, y, z, last_x, last_y, last_z;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        first_number = (EditText) findViewById(R.id.number1); // ID from component
-        second_number = (EditText) findViewById(R.id.number2);// ID from component
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        view_x = (TextView) findViewById(R.id.view_x);
+        view_y = (TextView) findViewById(R.id.view_y);
+        view_z = (TextView) findViewById(R.id.view_z);
+
+        if ((accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)) != null){
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+
         // The activity is created
     }
 
-    public void sum (View view){
-        int n1 = Integer.parseInt(first_number.getText().toString());
-        int n2 = Integer.parseInt(second_number.getText().toString());
-        int sum = n1 + n2;
-        Toast.makeText(this, "The sum is: " + sum, Toast.LENGTH_SHORT).show();
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
 
+            if(Math.abs(x - last_x) > 2 || Math.abs(x - last_x) > 2 || Math.abs(x - last_x) > 2){
+                Send();
+            }
+                view_x.setText("X: " + x);
+                view_y.setText("Y: " + y);
+                view_z.setText("Z: " + z);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public void Send()
+    {
+        Intent i = new Intent(this, MainActivity2.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
     }
 }
 
